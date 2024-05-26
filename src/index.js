da2e27c548daec771c0be835584d9c464d52050e
ahmed Dasso <65762595+DassoAhmed@users.noreply.github.com>
2024-05-26 04:09:52 +0100

    logout init

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './style.css';
 import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import { getAuth, EmailAuthProvider } from 'firebase/auth';
+import {
+    getAuth,
+    EmailAuthProvider,
+    signOut,
+    onAuthStateChanged
+  } from 'firebase/auth';
+  
 import {} from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
@@ -30,9 +36,15 @@ async function main() {
   const ui = new firebaseui.auth.AuthUI(auth);
 
   // Listen to RSVP button clicks
-  startRsvpButton.addEventListener("click",
-   () => {
-        ui.start("#firebaseui-auth-container", uiConfig);
+ // Called when the user clicks the RSVP button
+startRsvpButton.addEventListener('click', () => {
+    if (auth.currentUser) {
+      // User is signed in; allows user to sign out
+      signOut(auth);
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
   });
   // Add Firebase project configuration object here
   const firebaseConfig = {
@@ -68,5 +80,14 @@ async function main() {
   };
 
   // const ui = new firebaseui.auth.AuthUI(auth);
+
+  // Listen to the current Auth state
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      startRsvpButton.textContent = 'LOGOUT';
+    } else {
+      startRsvpButton.textContent = 'RSVP';
+    }
+  });
 }
 main();
